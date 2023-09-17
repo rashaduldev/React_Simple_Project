216cef2e0b4a8421867ecc6eb35573a21a9fa727
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-09-17 09:19:07 +0600

    Data Loded Done

--- src/components/Cources.jsx
+++ src/components/Cources.jsx
@@ -1,7 +1,13 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Cources = () => {
+    const [cources,setCources]=useState([]);
+    useEffect(()=>{
+        fetch('/Data.json')
+        .then(res=>res.json())
+        .then(data=>console.log(data))
+    },[])
     return (
         <div className='mx-10'>
             <h2 className='text-5xl text-center font-bold my-4'>Cource Registation</h2>
