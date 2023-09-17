595febd3f3693fd531c9aeb27fd0b3588a5be46a
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-09-17 13:35:56 +0600

    data transfer Done

---
+++ src/components/Courc.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Courc = ({clickTitle}) => {
+    console.log(clickTitle);
+
+    if (!Array.isArray(clickTitle)) {
+        // Handle the case when clickTitle is not an array.
+        return null; // Or render an appropriate error message.
+      }
+    
+    // const {title}=clickTitle;
+    return (
+        <div className='p-8 bg-slate-100 shadow-2xl rounded-lg mx-5'>
+        <h3 className='text-[#2F80ED] text-xl mb-3'>Credit Hour Remaining :hr</h3>
+         <hr />
+        
+        <h3 className='text-4xl font-bold my-4'>Cource Name</h3>
+      <div className='text-[#1c1b1b99] text-lg font-normal	mb-4'>
+      {
+        clickTitle.map((course,index)=>(
+          
+            <ul key={index}>
+            <li>{`${index + 1}. ${course.title}`}</li>
+          </ul>
+        
+        ))
+       }
+      </div>
+   <hr />
+   <h2 className='my-5 text-lg'>Total Credit Hour : </h2>
+   <hr />
+   <h2 className='text-xl'>Total Prices: USD</h2>
+       
+    </div>
+    );
+};
+
+export default Courc;
--- src/components/Cources.jsx
+++ src/components/Cources.jsx
@@ -1,19 +1,48 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
+import Courc from './Courc';
 
-const Cources = () => {
-    const [cources,setCources]=useState([]);
-    useEffect(()=>{
-        fetch('/Data.json')
-        .then(res=>res.json())
-        .then(data=>console.log(data))
-    },[])
-    return (
-        <div className='mx-10'>
-            <h2 className='text-5xl text-center font-bold my-4'>Cource Registation</h2>
-            <hr />
+const Courses = () => {
+  const [courses, setCourses] = useState([]);
+  const [clickTitle, setClickTitle] = useState([]);
+
+  useEffect(() => {
+    fetch('/Data.json')
+      .then((res) => res.json())
+      .then((data) => setCourses(data));
+  }, []);
+
+  const oldData=clickTitle.title
+  const handleClicked = (course) => {
+    
+    setClickTitle([course]);
+  };
+
+  return (
+    <div className='mx-10'>
+      <h2 className='text-5xl text-center font-bold my-4'>Course Registration</h2>
+      <hr />
+      <div className='flex flex-col lg:flex-row'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 mt-5 w-9/12 gap-5'>
+          {courses.map((course, index) => (
+            <div key={index} className="card card-compact bg-base-100 shadow-2xl p-4 rounded">
+              <figure><img className='w-full' src={course.img} alt="" /></figure>
+              <div className="card-body">
+                <h2 className="card-title">{course.title}</h2>
+                <p className='my-5'>{course.description}</p>
+                <div className="card-actions justify-end">
+                  <button onClick={() => handleClicked(course)} className="btn btn-primary bg-blue-700 text-white w-full py-3 rounded text-xl font-bold">Select</button>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-    );
+        <div className='w-3/12 shadow-2xl'>
+          <Courc clickTitle={clickTitle} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Cources;
+export default Courses;
